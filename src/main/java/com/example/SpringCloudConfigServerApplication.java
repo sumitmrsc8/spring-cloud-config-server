5abbbbde4701edbd282a81470a9f77462dca0960
sj <sj@gmail.com>
2016-08-28 10:39:28 -0700

    Spring Cloud Config Server

---
+++ src/main/java/com/example/SpringCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableAutoConfiguration
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+}
